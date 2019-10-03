011e14c087f3588e8030da5e6912b0026cddf6c9
namnorid.github.io <55981744+namnorid@users.noreply.github.com>
2019-10-03 03:41:27 +0200

    trettonde

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('#nav').load('/navbar.html');
+
+lightbox.option({
+	'albumLabel':	"picture %1 of %2",
+	'fadeDuration': 300,
+	'resizeDuration': 150,
+	'wrapAround': true
+})
+
+$(document).ready(function() {
+  $('#navbarDropdown').mouseenter(function() {
+    $('.dropdown-menu').slideToggle(300, "linear");
+  });
+  
+  $('.dropdown-menu').mouseleave(function() {
+    $(this).slideToggle(300, "linear");
+  });
+});
