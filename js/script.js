dbb54d682880c255c4eca829557da6db0442bc5e
namnorid.github.io <55981744+namnorid@users.noreply.github.com>
2019-10-05 00:10:05 +0200

    sjuandra

--- js/script.js
+++ js/script.js
@@ -1,29 +1,38 @@
-$('#nav').load('/navbar.html');
+$(document).ready(function() {
+  $('#navbarDropdown').mouseenter(function() {
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 768px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
 
 lightbox.option({
 	'albumLabel':	"picture %1 of %2",
 	'fadeDuration': 300,
 	'resizeDuration': 150,
 	'wrapAround': true
-})
-
-$(document).ready(function() {
-  $('#navbarDropdown').mouseenter(function() {
-    $('.dropdown-menu').slideToggle(300, "linear");
-  });
-  
-  $('.dropdown-menu').mouseleave(function() {
-    $(this).slideToggle(300, "linear");
-  });
 });
+      
 
-$(document).ready(function() {
-  $('#navbarDropup').mouseenter(function() {
-    $('.dropup-menu').slideToggle(300, "linear");
-  });
-  
-  $('.dropup-menu').mouseleave(function() {
-    $(this).slideToggle(300, "linear");
-  });
-});
+
 
